9bd397b4d816dc461059e92e6ebea9ddb60a88de
Al Wold <alwold@alwold.com>
2014-11-15 17:50:56 -0700

    add next round thing, with timer to turn on button after 5 sec

---
+++ Boast/NextRoundViewController.h
@@ -0,0 +1,13 @@
+//
+//  NextRoundViewController.h
+//  Boast
+//
+//  Created by Al Wold on 11/15/14.
+//  Copyright (c) 2014 FunCast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NextRoundViewController : UIViewController
+
+@end
---
+++ Boast/NextRoundViewController.m
@@ -0,0 +1,38 @@
+//
+//  NextRoundViewController.m
+//  Boast
+//
+//  Created by Al Wold on 11/15/14.
+//  Copyright (c) 2014 FunCast. All rights reserved.
+//
+
+#import "NextRoundViewController.h"
+
+@interface NextRoundViewController ()
+
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UIButton *goButton;
+
+@end
+
+@implementation NextRoundViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5*NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        NSLog(@"turning on the go button and stuff");
+        self.goButton.hidden = NO;
+        self.activityIndicator.hidden = YES;
+    });
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)goPressed:(id)sender {
+}
+
+@end
