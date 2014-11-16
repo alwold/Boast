f034836c95187d18ae326b26b21a0aa19accfbf3
Al Wold <alwold@alwold.com>
2014-11-16 10:53:28 -0700

    - add buttons on storyboard - add import

--- Boast/GameSetupViewController.h
+++ Boast/GameSetupViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GoogleCast.h>
 
 @interface GameSetupViewController : UIViewController <GCKDeviceScannerListener, UITableViewDelegate, UITableViewDataSource, GCKDeviceManagerDelegate>
 
